a626a3b4f0aa779df81ccaf78825c8518d0fa7ce
Fausto G. Tommasi <mmasito123@yahoo.com>
2016-10-03 09:21:47 -0500

    Threaded changes

--- cracker.c
+++ cracker.c
@@ -12,8 +12,6 @@ struct recurse_arg{
   int index;  
   int length;
   int thread_number;
-  char start;
-  char end;
   int num_threads;
   int loopsize;
 } ;
@@ -54,17 +52,19 @@ void* threaded_recurse(void* args){
   struct recurse_arg* arg = (struct recurse_arg*) args;
   const char allowed_alphabet[] = "abcdefghijklmnopqrstuvwxyz";
   const unsigned int allowed_alphabet_size = 26;
-  char current[256];
+  char* current = (char*)malloc(256*sizeof(char));
+  current[255] = '\0';
   char target[256];
-  strcpy(current,arg->current);
-  strcpy(target,arg->target);
+  
   int chunk_size = allowed_alphabet_size/arg->num_threads;
-  int thread_start = arg->thread_number * (chunk_size+1);
-  int thread_finish = arg->thread_number+1 * (chunk_size);
+  int thread_start =((arg->thread_number) * (chunk_size));
+  int thread_finish = ((arg->thread_number+1) * (chunk_size));
+  printf("%d: %d - %d\n",arg ->thread_number,thread_start, thread_finish);
   thread_finish = ((thread_finish > allowed_alphabet_size) ? allowed_alphabet_size : thread_finish);
+  
   int i;
   for(i=0;i<arg->loopsize;i++){
-    recursive(target,current,arg->index,i,thread_start,thread_finish);
+    recursive(arg->target,arg->current,arg->index,i,thread_start,thread_finish);
   }
 }
 
@@ -86,7 +86,8 @@ int main(int argc, char** argv){
   strcat(string,empty);
   char target[256];
   pthread_t pthreads[atoi(argv[1])];
-
+  struct recurse_arg* args[atoi(argv[1])];
+  char strings[atoi(argv[1])];
   int i;
 #if 0
   for(i =0; i<atoi(argv[2]); i++)
@@ -95,22 +96,28 @@ int main(int argc, char** argv){
 #endif
 
   for(i=0;i<atoi(argv[1]);i++){
-    struct recurse_arg arg;
+    struct recurse_arg* arg = 
+      (struct recurse_arg*)malloc(sizeof(struct recurse_arg));
+    char* current = (char*)malloc(sizeof(char)*256);
+    strings[i] = current;
+    args[i] = arg; 
     char  temp1[256];
     char temp2[256];
+    arg->current = current; 
+    arg->target = argv[3];
+    arg->index=1;
+    arg->length=5;
+    arg->thread_number = i;
+    arg->num_threads = atoi(argv[1]);
+    arg->loopsize = atoi(argv[2]);
+    pthread_create(&pthreads[i],NULL,threaded_recurse,(void*)arg);
 
-    arg.target = argv[3];
-    arg.current = string;
-    arg.index=1;
-    arg.length=5;
-    arg.thread_number = i;
-    arg.num_threads = atoi(argv[1]);
-    arg.loopsize = atoi(argv[2]);
-    pthread_create(&pthreads[i],NULL,threaded_recurse,(void*)&arg);
   }
 
  for(i=0;i<atoi(argv[1]);i++){
-    pthread_join(pthreads[i],NULL);
+   pthread_join(pthreads[i],NULL);
+   free(strings[i]);
+   free(args[i]); 
   }
 
   free(string);
